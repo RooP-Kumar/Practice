23e0842434075e8ea92838493e3e1fe68c9ba446
Roop Kumar <roopkm12@gmail.com>
2024-04-10 11:29:26 +0530

    Wrote program of selection sort

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,7 +1,22 @@
+import java.util.*;
+
 public class SelectionSort {
 	public static void main(String[] args) {
-		System.out.println("Hello World, This is the main function of this program");
-		System.out.println("Hello There, this is another line I added for testing purpose.");
+		int[] arr = new int[]{9, 7, 5, 2, 8};
+		sort(arr);
+	}
+	
+	public static void sort(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			int j = i;
+			while(j > 0 && nums[j] <= nums[j-1]) {
+				int temp = nums[j];
+				nums[j] = nums[j-1];
+				nums[j-1] = temp;
+				j--;
+			}
+		}
+		System.out.println(Arrays.toString(nums));
 	}
 }
 
